50909d755ad1ee49cc91cbeae54b81136ea52831
kaiyangjia <kaiyang.jia@ourwill.cn>
2016-02-25 13:53:58 +0800

    添加CombinationForm

---
+++ library/src/main/java/com/jiakaiyang/library/easyform/core/CombinationForm.java
@@ -0,0 +1,7 @@
+package com.jiakaiyang.library.easyform.core;
+
+/**
+ * Created by kaiyangjia on 2016/2/25.
+ */
+public class CombinationForm {
+}
--- library/src/main/java/com/jiakaiyang/library/easyform/core/FormBuilder.java
+++ library/src/main/java/com/jiakaiyang/library/easyform/core/FormBuilder.java
@@ -2,6 +2,7 @@ package com.jiakaiyang.library.easyform.core;
 
 /**
  * Created by kaiyangjia on 2016/2/24.
+ * 用基础的表格组件FormView根据一定的条件构建复杂的表格
  */
 public class FormBuilder {
 }
